32e02f8f1796f46e8d234382824d340a4a453d07
Michael Maltese <mike@mikemaltese.com>
2010-06-20 13:02:39 -0700

    Refactored common code in sdl

--- sdl.c
+++ sdl.c
@@ -2,46 +2,39 @@
 #include "sdl.h"
 
 SDL_Surface*
-c16_get_sprite_sdl (c16_t *image, uint16_t index)
+make_surface (uint16_t *data, int width, int height, c16_format_t format)
 {
-  c16_sprite_t sprite = c16_get_sprite (image, index);
-  c16_format_t format = c16_get_format (image);
-  uint16_t width, height, *data;
-  width = c16_sprite_get_width (sprite);
-  height = c16_sprite_get_height (sprite);
-  data = c16_sprite_get_data (sprite);
-  
 	unsigned int rmask, gmask, bmask;
 	if (format == C16_565)
-		rmask = 0xF800, gmask = 0x07E0, bmask = 0x001F;
+		rmask = 0xf800, gmask = 0x07e0, bmask = 0x001f;
 	else
-		rmask = 0x7C00, gmask = 0x03E0, bmask = 0x001F;
+		rmask = 0x7c00, gmask = 0x03e0, bmask = 0x001f;
   
 	return SDL_CreateRGBSurfaceFrom(
 	  data, width, height,
 		16, // depth
 		width * 2, // pitch
-		rmask, gmask, bmask, 0); // RGBA mask
+		rmask, gmask, bmask, 0);
+}
+
+SDL_Surface*
+c16_get_sprite_sdl (c16_t *image, uint16_t index)
+{
+  c16_sprite_t sprite = c16_get_sprite (image, index);
+  
+  return make_surface (
+    c16_sprite_get_data (sprite),
+    c16_sprite_get_width (sprite),
+    c16_sprite_get_height (sprite),
+    c16_get_format (image));
 }
 
 SDL_Surface*
 blk_get_sdl (blk_t *image)
 {
-  c16_format_t format = image->format;
-  uint16_t width, height, *data;
-  width = image->width;
-  height = image->height;
-  data = image->data;
-  
-	unsigned int rmask, gmask, bmask;
-	if (format == C16_565)
-		rmask = 0xF800, gmask = 0x07E0, bmask = 0x001F;
-	else
-		rmask = 0x7C00, gmask = 0x03E0, bmask = 0x001F;
-  
-	return SDL_CreateRGBSurfaceFrom(
-	  data, width, height,
-		16, // depth
-		width * 2, // pitch
-		rmask, gmask, bmask, 0); // RGBA mask
+  return make_surface (
+    blk_get_data (image),
+    blk_get_width (image),
+    blk_get_height (image),
+    blk_get_format (image));
 }
